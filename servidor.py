52d54d516a9ff1c9218c3dd7f64323d0d507de4b
techyberme <107142731+techyberme@users.noreply.github.com>
2025-11-24 10:16:15 +0100

    Enhance comments in servidor.py for clarity
    Added comments for better code understanding and clarity.

--- servidor.py
+++ servidor.py
@@ -5,17 +5,18 @@ IP = "0.0.0.0"   #cualquier red conectada al servidor
 PORT = 5006
 buffer_size= 1024
 clients = []
-
+#Función de manejo de cliente
 def clientes(conn, addr):
     print(f"Nuevo cliente conectado: {addr}")
+    #añade el nuevo cliente a la lista
     clients.append(conn)
     try:
         while True:
-            data = conn.recv(buffer_size)
+            data = conn.recv(buffer_size)   #recepción de datos
             if not data:
                 break
 
-           
+           #decodificado del mensaje
             msg = data.decode().strip()
             print(f"[{addr}] → {msg}")
             # Reenviar el mensaje a TODOS los clientes excepto el que lo manda
@@ -25,14 +26,14 @@ def clientes(conn, addr):
 
     except:
         pass
-
+    #rutina de desconexión
     print(f" Cliente desconectado: {addr}")
-    clients.remove(conn)
+    clients.remove(conn)  #se quita el cliente de la lista y se cierra la conexión.
     conn.close()
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((IP, PORT))
+server.bind((IP, PORT))  #se crea el servidor en la red
 server.listen(5)
 
 print(f" Servidor escuchando en {PORT}...")
