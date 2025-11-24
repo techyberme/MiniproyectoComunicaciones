8533a6981895c0a777f2a43515792ac7bc111c15
techyberme <107142731+techyberme@users.noreply.github.com>
2025-11-24 10:18:21 +0100

    Implement message validation before sending
    Added input validation for messages sent to the server.

--- cliente.py
+++ cliente.py
@@ -2,7 +2,7 @@ import socket
 
 SERVER_IP = "172.20.10.5"   # PC servidor
 PORT = 5006
-
+#conexión al servidor
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER_IP, PORT))
 
@@ -11,12 +11,13 @@ print("Conectado al servidor...")
 try:
     while True:
         msg = ""
+        #solo se pueden enviar estos mensajes
         while msg != "LED=ON" and msg != "LED=OFF" and msg != "ESTADO":
             msg = input("Comando a enviar (LED=ON / LED=OFF / ESTADO): ")
-
+        #envío del mensaje
         client.sendall((msg + "\n").encode())
 
-        # Recibir datos del servidor (del ESP32)
+        # Recibir datos del servidor 
         data = client.recv(1024)
         if data:
             print("Servidor: ", data.decode().strip())
